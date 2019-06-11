2498df22bd775eff1f9b249f7b8107e7d43d2ebf
clement-hamon <clementhamon@clements-MacBook-Air.local>
2019-06-11 20:40:49 +0800

    euler #1 doc

--- setState-Ramda.js
+++ setState-Ramda.js
@@ -1,7 +1,11 @@
+  /**
+   * Allow the developer to modify an object by either passing a function or a value
+   */
   const functionalize = unless(is(Function), always);
   
   const functionalEvolve = object => compose(evolve(R.__, object), map(functionalize));
 
+  // example
   let state = {
     name: "toto",
     age: 32
